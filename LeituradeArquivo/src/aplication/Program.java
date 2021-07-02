12fd66ae717f13bc13a0f052933bfacadba1c527
Ekllipse <matheusaraujo1995m2@gmail.com>
2021-07-01 21:02:19 -0300

    Ler Arquivos em java v1

---
+++ LeituradeArquivo/src/aplication/Program.java
@@ -0,0 +1,28 @@
+package aplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		File file = new File("F:\\WORKSPACE JAVA\\LeituradeArquivo\\in.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}
+		
+	}
+
+}
