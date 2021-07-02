e18a7d9396ce9c82cdf3cf63b646064340123d43
Ekllipse <matheusaraujo1995m2@gmail.com>
2021-07-01 21:16:16 -0300

    Leitura de arquivos em java V2

--- LeituradeArquivo/src/aplication/Program.java
+++ LeituradeArquivo/src/aplication/Program.java
@@ -1,28 +1,26 @@
 package aplication;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 public class Program {
 
 	public static void main(String[] args) {
-		File file = new File("F:\\WORKSPACE JAVA\\LeituradeArquivo\\in.txt");
-		Scanner sc = null;
 		
-		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+		String path = "F:\\WORKSPACE JAVA\\LeituradeArquivo\\in.txt";
+
+		
+		try (BufferedReader bf = new BufferedReader(new FileReader(path))){
+			String line = bf.readLine();
+			while(line != null) {
+				System.out.println(line);
+				line = bf.readLine();
 			}
 		}catch (IOException e) {
 			System.out.println("Erro: " + e.getMessage());
-		}finally {
-			if(sc != null) {
-				sc.close();
-			}
 		}
-		
 	}
 
 }
